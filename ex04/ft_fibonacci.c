d90be81fd275b383188aa9372d0ba4e690f1f82b
Shanaya Jain <shjain@e1z2r7p26.42.us.org>
2018-07-16 11:25:21 -0700

    need to finish

---
+++ ex04/ft_fibonacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int ft_fibonacci(int index) {
+    if (index <= 1){
+        return index;
+    }
+    return ft_fibonacci(index-1) + ft_fibonacci(index+2);
+}
+
+
+int main(){
+    printf("The element at index %d of the Fibonacci sequence is %d\n", 4, ft_fibonacci(4));
+}
