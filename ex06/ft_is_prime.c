a6efadc58239308fa15a8e2d43cfa13b60c31989
Shanaya Jain <shanayajain2001@gmail.com>
2018-07-16 19:52:41 -0700

    day 4 home

--- ex06/ft_is_prime.c
+++ ex06/ft_is_prime.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-int ft_is_prime(int nb){
-int prime = 1;
-if (nb <= 1) {
-    prime = 0;
+#include <stdlib.h>
+
+int ft_is_prime(int nb) {
+    int is_prime = 1;
+    if (nb == 1 || nb == 0) {
+        is_prime = 0;
+    }
+    for (int i = 2; i <= nb / 2; i++) {
+        if (nb % i == 0) {
+            is_prime = 0;
+            break;
+        }
+    }
+    return is_prime;
 }
 
-for (int i = 2; i <= nb / 2; i++){
-if ((nb % i == 0)){
-    prime = 0;
-    break;
-}
-}
-
-return prime;
-}
-
-int main (void){
-    printf("Is %d prime?: %d\n", 11, ft_is_prime(11));
-    return 0;
+int main(int argc, char *argv[]) {
+ 
+    int num = atoi(argv[1]);
+    printf("Is %d prime?: %d\n", num, ft_is_prime(num));
 }
