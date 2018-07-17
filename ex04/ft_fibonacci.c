0e9d90445774aa7a5f5ce7ca2e8d376a1cdc531c
Shanaya Jain <shjain@e1z2r7p26.42.us.org>
2018-07-17 10:28:19 -0700

    fib

--- ex04/ft_fibonacci.c
+++ ex04/ft_fibonacci.c
@@ -4,10 +4,11 @@ int ft_fibonacci(int index) {
     if (index <= 1){
         return index;
     }
-    return ft_fibonacci(index-1) + ft_fibonacci(index+2);
+
+    return ft_fibonacci(index-1) + ft_fibonacci(index-2);
 }
 
 
 int main(){
-    printf("The element at index %d of the Fibonacci sequence is %d\n", 4, ft_fibonacci(4));
+    printf("The element at index %d of the Fibonacci sequence is %d\n", 1, ft_fibonacci(1));
 }
