61627636e0e690e70bef444f45e0379655e8c0b7
Shanaya Jain <shanayajain2001@gmail.com>
2018-07-16 19:39:27 -0700

    day four at home

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 int ft_iterative_power(int nb, int power){
+    if (power < 0){
+        return 0;
+    }
     int product = 1;
     for (int i = 1; i <= power; i++){
         product = product * nb;
     }
-    if (power < 0){
-        return 0;
-    }
-    else {
-        return product;
-    }
+    return product;
 }
 
 int main(){
-    printf("%d to the power of %d: %d\n", 4, 3, ft_iterative_power(4, 3));
+    printf("%d to the power of %d: %d\n", 4, 0, ft_iterative_power(4, 0));
 }
