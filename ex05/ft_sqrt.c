2ffb20fd9c322227ce66665568c063c56c08bd2b
Shanaya Jain <shanayajain2001@gmail.com>
2018-07-17 22:25:25 -0700

    square root home

--- ex05/ft_sqrt.c
+++ ex05/ft_sqrt.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int ft_sqrt(int nb) {
-if 
+    int min = 1;
+    int max = nb;
+    do {
+        long current = (min + max) / 2;
+        long square = current * current;
+        if (square == nb)
+           return current;
+        else if (square > nb)
+           max = current;
+        else
+           min = current;
+    } while (min + 1 < max);
+    return 0;
 }
 
-int main(){
-    printf("%d\n", 4, ft_sqrt(4));
+int main(int argc, char *argv[]) {
+ 
+    int num = atoi(argv[1]);
+    printf("The square root of %d is %d\n", num, ft_sqrt(num));
 }
